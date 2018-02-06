ab2dba2cde1a46fd4e85ac37bd2258fc28d07adc
gediminask <tarkim.gediminas@gmail.com>
2018-02-06 12:59:43 +0200

    Vue charts added

---
+++ app/Repositories/Dashboard/DashboardRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gediminask
+ * Date: 18.2.6
+ * Time: 10.15
+ */
+
+namespace App\Repositories\Dashboard;
+
+
+interface DashboardRepository
+{
+
+}
---
+++ app/Repositories/Dashboard/EloquentDashboard.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gediminask
+ * Date: 18.2.6
+ * Time: 10.16
+ */
+
+namespace App\Repositories\Dashboard;
+
+
+class EloquentDashboard
+{
+
+}
